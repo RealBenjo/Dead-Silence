bc41039cc44dc499204aa6ea8dc2b54d90cd7c9c
Benjamin <benjamin.ipavec@gmail.com>
2026-03-19 10:54:42 +0100

    uuuhm
    Jesus Christ help me

--- scripts/enemies/Zombie.gd
+++ scripts/enemies/Zombie.gd
@@ -1,8 +1,13 @@
 extends EnemyWalking
 
 
+@onready var state_machine: Node = $StateMachine
+
 # premade signals
 
 # calculate path finding only every 0.5s
 func _on_path_find_timer_timeout() -> void:
 	make_path(interest_pos)
+
+# make helper object that emits signal eevery frame
+# so you can have multiple update functions in one object pretty much
