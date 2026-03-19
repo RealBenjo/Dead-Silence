9f67f530a930f6bc4a2f35c36b433726ccf33fa1
Benjamin Ipavec <benjamin.ipavec@gmail.com>
2026-03-19 21:12:13 +0100

    better anims + for zombie and player + made the frames lower resolution to save on disk space

--- scripts/enemies/Zombie.gd
+++ scripts/enemies/Zombie.gd
@@ -2,6 +2,7 @@ extends EnemyWalking
 
 
 @onready var state_machine: Node = $StateMachine
+@onready var animation: AnimatedSprite2D = $AnimatedSprite2D
 
 # premade signals
 
@@ -11,3 +12,11 @@ func _on_path_find_timer_timeout() -> void:
 
 # make helper object that emits signal eevery frame
 # so you can have multiple update functions in one object pretty much
+
+func _on_updater_fixed_update(_delta: float) -> void:
+	var cur_state = state_machine.current_state.get_script().get_global_name()
+	
+	if velocity == Vector2.ZERO:
+		animation.play("idle")
+	else:
+		animation.play("walking")
